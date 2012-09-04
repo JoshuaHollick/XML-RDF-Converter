1407a4714fc5d3a6e73ea0b568491951ada2f500
Cameron Fitzgerald <camandco@gmail.com>
2012-09-05 00:10:07 +0800

    more working code

--- readxml.java
+++ readxml.java
@@ -21,68 +21,66 @@ public class readxml {
 			
 			//parses the XML code into the DOM
 			Document doc = dBuilder.parse(fXmlFile);
-	
+			doc.getDocumentElement().normalize();
 	
 			//Read in the document encapsulating element (Should be gmd:MD_Metadata for us)
-			Element root = doc.getDocumentElement();  
+			Node root = doc.getDocumentElement();  
 			System.out.println("The root element is " + root.getNodeName() + ".\n"); //Node Name is gmd:MD_Metadata.
 			
-			       
-			NodeList children = root.getChildNodes();  
-			System.out.print("There are "+children.getLength()+" child elements.\n");  
-			System.out.print("They are: \n");  
-	
-			for (Node child = root.getFirstChild();child != null;child = child.getNextSibling()) { 
-				//recurseLevel(child); 
-				if (child.getNodeType() == child.TEXT_NODE){  
-					System.out.println("Text: "+child.getNodeValue());  
-				} 
-				else if (child.getNodeType() == child.ELEMENT_NODE) {  
-					System.out.println(child.getNodeName()+" = "+child.getFirstChild().getNodeValue());  
-					//   if()  
-				}  
-			} 
-
+			NodeList emptyTextNodes = doc.getElementsByTagName("\n"); 
+			
+			for (int i = 0; i < emptyTextNodes.getLength(); i++) {
+				Node emptyTextNode = emptyTextNodes.item(i);
+				emptyTextNode.getParentNode().removeChild(emptyTextNode);
+			}
+			
+			//Begin recursing on children of that element.
+			recurseLevel(root, 0);
+			
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
 
-private static String recurseLevel(Element root) {
-try {
-//read the "root" element and check if it has children
+private static String recurseLevel(Node root, int level) {
+	try {       
+		int recurse = 1;
+	
+		//Character string type used frequently.
+		if(root.getNodeName() == "gco:CharacterString") {
+			for(int i=0; i<=level; i++) System.out.print("\t");
+			System.out.println("Node (level "+level+"):"+root.getNodeName()+" = "+root.getFirstChild().getNodeValue());
+			recurse = 0;
+		}
+		else if(root.getNodeType() != root.TEXT_NODE) {
+			for(int i=0; i<=level; i++) System.out.print("\t");
+			System.out.println("Node (level "+level+"):"+root.getNodeName()+" - "+root.getNodeValue());
+		}
+		else recurse = 0;
+	
+		//Only recurse for certain node types (i.e. not gco:CharacterString)
+		if(recurse == 1) {
+			//Recurse through the children of this node.
+			NodeList children = root.getChildNodes();  
+	
+			for (Node child = root.getFirstChild(); child != null; child = child.getNextSibling()) {  
+				recurseLevel(child, level+1);
+			} 
+		}
 
-//if it has one child, find the text element within that child
-
-//else recurse
-
-System.out.println("The root element is " + root.getNodeName() + ".\n");  
-       
-NodeList children = root.getChildNodes();  
-System.out.print("There are "+children.getLength()+" child elements.\n");  
-System.out.print("They are: \n");  
-
-for (Node child = root.getFirstChild();child != null;child = child.getNextSibling()) {  
-if (child.getNodeType() == child.TEXT_NODE){  
-System.out.println("Text: "+child.getNodeValue());  
-} else if (child.getNodeType() == child.ELEMENT_NODE) {  
-System.out.println(child.getNodeName()+" = "+child.getFirstChild().getNodeValue());  
-//   if()  
-}  
-} 
-
-} catch (Exception e) {
-e.printStackTrace();
+	} catch (Exception e) {
+		e.printStackTrace();
+	}
+	
+	return "";
 }
-}
-}
 
-private static String getTagValue(String sTag, Element eElement) {
-NodeList nlList = eElement.getElementsByTagName(sTag).item(0).getChildNodes();
+//private static String getTagValue(String sTag, Element eElement) {
+//NodeList nlList = eElement.getElementsByTagName(sTag).item(0).getChildNodes();
 
-Node nValue = (Node) nlList.item(0);
-
-return nValue.getNodeValue();
-}
+//Node nValue = (Node) nlList.item(0);
+//
+//return nValue.getNodeValue();
+//}
 
 }
