364f139f680e8eea9bf40edc5eefed0e05efa64a
Josh <20268011@student.uwa.edu.au>
2012-09-17 10:14:45 +0800

    updated JenaInterface and XMLDocument

--- JenaInterface.java
+++ JenaInterface.java
@@ -3,15 +3,22 @@
  * @author Joshua Hollick
  */
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
 import java.io.InputStream;
 import java.io.PrintStream;
+
+import java.io.IOException;
 import java.io.FileNotFoundException;
+
 import java.util.TreeMap;
 
 import com.hp.hpl.jena.rdf.model.Model;
 import com.hp.hpl.jena.rdf.model.ModelFactory;
 import com.hp.hpl.jena.rdf.model.Resource;
 //import com.hp.hpl.jena.util.FileManager;
+
 
 public class JenaInterface {
 	private Model model;
@@ -62,26 +69,70 @@ public class JenaInterface {
 		return true;
 	}
 	
-	public Boolean readPredicatesFromFile(String Filename) {
+	public Boolean readPredicatesFromFile(String filename) {
 		/**
 		 * Reads predicates from file and store them in a map for use when parsing the XML
 		 */
 		
-		// Set prefixes with:
-		//	model.setNsPrefix("Prefix string ie vcard", "Full URI ie http://www.w3.org/2001/vcard-rdf/3.0#");
+		File file = new File(filename);
+		StringBuffer contents = new StringBuffer();
+		BufferedReader reader = null;
+
+		try {
+			reader = new BufferedReader(new FileReader(file));
+			String text = null;
+
+			System.out.println("Reading prefixes\n");
+			
+			// Read prefixes
+			while ((text = reader.readLine()) != null) {
+				if(text.isEmpty()) break;
+				if(text.charAt(0) == '#') continue;		// Comment Line
+
+				// TODO; save the prefixes
+				//model.setNsPrefix("Prefix string ie vcard", "Full URI ie http://www.w3.org/2001/vcard-rdf/3.0#");
+			}
+			
+			System.out.println("Reading predicates\n");
+			
+			// Read predicates
+			while ((text = reader.readLine()) != null) {
+				if(text.isEmpty()) continue;
+				if(text.charAt(0) == '#') continue;		// Comment Line
+
+				// TODO; Save the predicates
+				//	predicateMap.put("Key used in XML", "RDF prefix");
+			}
+			
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				if (reader != null) {
+					reader.close();
+				}
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
 		
-		// Store predicates with:
-		//	predicateMap.put("Key used in XML", "RDF prefix");
-		
-		
-		return false;
+		return true;
 	}
 	
-	public void InsertGeoNetworkXMLObject() {
+	public void InsertGeoNetworkXMLObject(XMLDocument xmlDoc) {
+		// TODO: Fix this
+		Resource documentRoot = model.createResource("<URI>");
 		
+		Resource contactResource = documentRoot.addProperty(model.createProperty("<contactPredicate>"), model.createResource());
 		
-		Resource contactResource = model.createResource(contact.individualName_ns + "/" + contact.individualName);
+		contactResource.addProperty(model.createProperty(xmlDoc.contact.get("individualName_ns")), xmlDoc.contact.get("individualName"));
 		
+		// TODO: Iterate throuch contact
 		
+//		Resource identificationInfoResource = documentRoot createResource(...);
+		
+		// TODO: Iterate through identificationInfo
 	}
 }
--- XMLDocument.java
+++ XMLDocument.java
@@ -1,8 +1,12 @@
 import java.util.HashMap;
+import java.util.Vector;
 
 public class XMLDocument {
+	HashMap<String, String> misc;		// Your "things" list
+	
 	HashMap<String, String> contact;
 	HashMap<String, String> identificationInfo;
-	HashMap<String, String> misc;
+	HashMap<String, String> dataQualityInfo;
 	
+	Vector<HashMap<String, String>> onlineResources;
 }
