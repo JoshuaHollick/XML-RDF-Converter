9b8078fbed4c8ff04719c91545af491797bea50c
Wahhaj Ali <wahhajj@gmail.com>
2012-09-25 11:18:48 +0800

    Organized the directory further

---
+++ JenaInterface/TestJenaRDFModel.java
@@ -0,0 +1,8 @@
+public class TestJenaRDFModel {
+	public static void main(String args[]) {
+		JenaRDFModel ji = new JenaRDFModel("test.rdf", "http://www.isotc211.org/2005/gmd/gmd.xsd");
+
+		// ji.readPredicatesFromFile("predicates.txt");
+		ji.writeModelToFile("TestModel.xml");
+	}
+}
--- TestJenaRDFModel.java
+++
@@ -1,8 +0,0 @@
-public class TestJenaRDFModel {
-	public static void main(String args[]) {
-		JenaRDFModel ji = new JenaRDFModel("test.rdf", "http://www.isotc211.org/2005/gmd/gmd.xsd");
-
-		// ji.readPredicatesFromFile("predicates.txt");
-		ji.writeModelToFile("TestModel.xml");
-	}
-}
